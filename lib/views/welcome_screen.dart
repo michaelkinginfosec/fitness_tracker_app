598b9ed412dd56d4617a68ce4663ba859c4d1fcd
michaelkinginfosec <osundemichael7@gmail.com>
2024-07-11 19:01:33 +0100

    update

--- lib/views/welcome_screen.dart
+++ lib/views/welcome_screen.dart
@@ -1,9 +1,7 @@
 import 'package:fitness_tracker_app/components/categories_container.dart';
 import 'package:fitness_tracker_app/components/custom_app_bar.dart';
-import 'package:flutter/cupertino.dart';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
@@ -25,20 +23,20 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
         calenderIcon: () {},
       ),
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
           children: [
-            SizedBox(
+            const SizedBox(
               height: 30,
             ),
-            Text(
+            const Text(
               'Fitnes \nTracking Device',
               style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
             ),
-            SizedBox(
+            const SizedBox(
               height: 20,
             ),
-            Row(
+            const Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 CategoryContainer(
@@ -52,41 +50,34 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 ),
               ],
             ),
-            SizedBox(
-              height: 20,
-            ),
-            Column(
-              children: [
-                Stack(
-                  children: [
-                    Positioned(
-                      top: 20,
-                      left: 30,
-                      child: Container(
-                        height: 50,
-                        width: double.infinity,
-                        color: Colors.blue,
-                      ),
-                    ),
-                    Positioned(
-                      child: Container(
-                        height: 50,
-                        width: double.infinity,
-                        color: Colors.red,
-                      ),
-                    ),
-                    Positioned(
-                      left: -20,
-                      child: Container(
-                        height: 50,
-                        width: double.infinity,
-                        color: Colors.green,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            )
+            // Stack(
+            //   children: [
+            //     Positioned(
+            //       top: 20,
+            //       left: 30,
+            //       child: Container(
+            //         height: 50,
+            //         width: double.infinity,
+            //         color: Colors.blue,
+            //       ),
+            //     ),
+            //     // Positioned(
+            //     //   child: Container(
+            //     //     height: 50,
+            //     //     width: double.infinity,
+            //     //     color: Colors.red,
+            //     //   ),
+            //     // ),
+            //     // Positioned(
+            //     //   left: -20,
+            //     //   child: Container(
+            //     //     height: 50,
+            //     //     width: double.infinity,
+            //     //     color: Colors.green,
+            //     //   ),
+            //     // ),
+            //   ],
+            // )
           ],
         ),
       ),
